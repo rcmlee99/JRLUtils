1bd63e466ee516428f0b19c824340f523e2a39aa
Roger Lee <rcmlee99@gmail.com>
2017-01-31 11:47:25 +1100

    Category for finding the visible view controller in UIWindow, present alert with dismiss action style using Modal Presentation Popover style

---
+++ JRLUtils/UIAlertController+JRLUtils.h
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Created by Roger Lee on 31/01/2017.
+// Copyright (c) 2017 JRLee Pty Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import <UIKit/UIKit.h>
+#import "JRLCommon.h"
+
+@interface UIAlertController (JRLUtils)
+
+// Instantiate the Alert Controller with title, message, preferred style and array of UIAlertActions.
+// Sets the alert controller to Modal Presentation Popover style. The Popover Controller will be presented in the view and with offset x,y
++ (instancetype)jrl_alertWithTitle:(NSString *)title
+                           message:(NSString *)message
+                    preferredStyle:(UIAlertControllerStyle)preferredStyle
+                            inView:(UIView*)view
+                            offset:(CGPoint)point
+                           actions:(NSArray *)actions;
+
+// Instantiate the Alert Controller with title, message, preferred style and array of UIAlertActions.
+// Sets the alert controller to Modal Presentation Popover style. The Popover Controller will be presented in the view and with offset -10,50
++ (instancetype)jrl_alertWithTitle:(NSString *)title
+                           message:(NSString *)message
+                    preferredStyle:(UIAlertControllerStyle)preferredStyle
+                            inView:(UIView*)view
+                           actions:(NSArray *)actions;
+
+@end
---
+++ JRLUtils/UIAlertController+JRLUtils.m
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Created by Roger Lee on 31/01/2017.
+// Copyright (c) 2017 JRLee Pty Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import "UIAlertController+JRLUtils.h"
+
+@implementation UIAlertController (JRLUtils)
+
+#pragma mark - Public
+
++ (instancetype)jrl_alertWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)preferredStyle inView:(UIView*)view actions:(NSArray *)actions {
+    
+    return [self jrl_alertWithTitle:title message:message preferredStyle:preferredStyle inView:view offset:CGPointMake(-10, 50) actions:actions];
+}
+
++ (instancetype)jrl_alertWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)preferredStyle inView:(UIView*)view offset:(CGPoint)point actions:(NSArray *)actions {
+    
+    UIAlertController * alertController = [UIAlertController alertControllerWithTitle:title
+                                                                              message:message
+                                                                       preferredStyle:preferredStyle];
+    
+    for (UIAlertAction *action in actions) {
+        [alertController addAction:action];
+    }
+    
+    [alertController setModalPresentationStyle:UIModalPresentationPopover];
+    UIPopoverPresentationController *popoverController = [alertController
+                                                          popoverPresentationController];
+    popoverController.sourceView = view;
+    popoverController.sourceRect = CGRectOffset([view bounds],point.x,point.y);
+    popoverController.permittedArrowDirections = 0;
+    
+    return alertController;
+}
+
+
+@end
